14a5fdf19715fc5c75752194e15cae7bc167e4e8
Shubham Awchare <67494871+shubhamawchare@users.noreply.github.com>
2026-08-10 19:03:02 +0530

    Time: 13 ms (53.10%) | Memory: 12.3 MB (73.96%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        m = len(s)
+        n = len(p)
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        # Patterns like a*, a*b*, a*b*c* can match an empty string
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                # Direct character match or '.'
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+
+                # '*' means zero or more of previous character
+                elif p[j - 1] == '*':
+                    # Zero occurrences
+                    dp[i][j] = dp[i][j - 2]
+
+                    # One or more occurrences
+                    if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+        return dp[m][n]
